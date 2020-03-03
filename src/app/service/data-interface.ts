f270bcf2568a3d12a7dcdb5d267785840b3cab44
unknown <karol.sujeta@outlook.com>
2020-03-03 01:13:15 +0100

    v2-map-works

--- src/app/service/data-interface.ts
+++ src/app/service/data-interface.ts
@@ -1,11 +1,5 @@
 export class DataInterface {
-    public name: string;
-    public population: number;
-    public climate: string;
-
-    constructor(name: string, population: number, climate: string) {
-        this.name = name;
-        this.population = population;
-        this.climate = climate;
-    }
+    constructor(public name: string,
+        public population: number,
+        public climate: string) { }
 }
